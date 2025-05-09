0b64781589ea3c8d0643a90ca348b202a6b772a1
ExtremeCrazyLarp <147116067+ExtremeCrazyLarp@users.noreply.github.com>
2025-05-08 21:14:06 -0400

    updated talkingBen.html
    finished the talking ben activity

--- talkingBen_ACTIVITY/script.js
+++ talkingBen_ACTIVITY/script.js
@@ -10,9 +10,40 @@ function reply(id) {
         case 3:
             return "ugh";
         case 4:
-            return "ho ho ho";
+            return "hohoho";
         default:
-            return "ERROR null id";
+            return "hangup";
+    }
+}
+
+function replyGif(id) {
+    // show appropriate ben reaction gif
+    var reactionGif;
+    switch (id) {
+        case 0:
+            reactionGif = document.getElementById('benYes');
+            reactionGif.style.display = "flex";
+            break;
+        case 1:
+            reactionGif = document.getElementById('benNo');
+            reactionGif.style.display = "flex";
+            break;
+        case 2:
+            reactionGif = document.getElementById('benBen');
+            reactionGif.style.display = "flex";
+            break;
+        case 3:
+            reactionGif = document.getElementById('benUgh');
+            reactionGif.style.display = "flex";
+            break;
+        case 4:
+            reactionGif = document.getElementById('benHoHoHo');
+            reactionGif.style.display = "flex";
+            break;
+        default:
+            reactionGif = document.getElementById('benPhone');
+            reactionGif.style.display = "flex";
+            break;
     }
 }
 
@@ -20,6 +51,7 @@ function reply(id) {
 function response() {
     // get response text box (text output is temporary, gif+audio response is end goal)
     var output = document.getElementById('benResponse');
+    
     // get the random response ID number
     var responseID;
     if (document.getElementById('forceYesNo').checked) {
@@ -30,24 +62,56 @@ function response() {
         responseID = Math.floor(Math.random() * 5);
     }
     var responseText = reply(responseID);
+    var responseAudio = document.getElementById(responseText);
+
     output.innerText = "Ben says: " + responseText;
+    responseAudio.play();
+    replyGif(responseID);
+}
+
+function clearReplyText() {
+    var reply = document.getElementById('benResponse');
+    reply.innerText = "";
+}
+
+function clearGifs() {
+    var currGif;
+    // clear initial gif
+    currGif = document.getElementById('benPhone');
+    currGif.style.display = "none";
+
+    // clear output gifs
+    currGif = document.getElementById('benYes');
+    currGif.style.display = "none";
+    currGif = document.getElementById('benNo');
+    currGif.style.display = "none";
+    currGif = document.getElementById('benBen');
+    currGif.style.display = "none";
+    currGif = document.getElementById('benUgh');
+    currGif.style.display = "none";
+    currGif = document.getElementById('benHoHoHo');
+    currGif.style.display = "none";
 }
 
 // read user input out loud
 function tts() {
+    clearGifs();
+    clearReplyText();
     // get user input from doc
-    var input = "Hey Ben, I was wondering..." + document.getElementById('userPrompt').value;
-    // init temp output for user
-    var tmpOut = document.getElementById('tempOutput');
+    var input = "Hey Ben, " + document.getElementById('userPrompt').value;
     // initialize tts api
     var speech = new SpeechSynthesisUtterance(input);
     speech.volume = 1;
-    speech.rate = 1;
+    speech.rate = 1.5;
     speech.pitch = 1;
     // read input out
     window.speechSynthesis.speak(speech);
-    // ben response
-    response();
-    // temp output for user (wip message)
-    tmpOut.innerText = "Sorry thats all I have to offer right now, thanks for coming here though :)";
+    speech.onend = function() {
+        response();
+    }
+}
+
+function answerAudio() {
+    var sound = document.getElementById('answer');
+    sound.play();
 }
